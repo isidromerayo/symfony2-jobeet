9c6872e7eeb9c27b7e39d312a80f102573fd4fca
Isidro Merayo Castellano <imerayo@saludcastillayleon.es>
2012-08-07 14:24:52 +0200

    add test to show job

--- src/Hcuv/JobeetBundle/Tests/Controller/JobControllerTest.php
+++ src/Hcuv/JobeetBundle/Tests/Controller/JobControllerTest.php
@@ -30,6 +30,11 @@ class JobControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/job/');
         $this->assertTrue(200 === $client->getResponse()->getStatusCode());
         $this->assertTrue($crawler->filter('td.position')->count() > 0);
+        // TODO better dom expression
+        $link = $crawler->selectLink('Web Designer')->link();
+        $crawler = $client->click($link);
 
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode(), 'Don\'t load detail job');
+        $this->assertTrue($crawler->filter('h1:contains("Job")')->count() == 1);
     }
 }
