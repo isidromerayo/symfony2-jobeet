8a2ff7ad40c1258778dc4709014d4e9da433b951
Isidro Merayo Castellano <imerayo@saludcastillayleon.es>
2012-08-09 13:25:00 +0200

    Job Category Controller Creation and fixed route to category

---
+++ src/Hcuv/JobeetBundle/Controller/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hcuv\JobeetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Hcuv\JobeetBundle\Entity\Category;
+/**
+ * Category controller
+ *
+ * User: imerayo
+ * Date: 8/9/12
+ * Time: 1:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class CategoryController extends Controller
+{
+
+}
