0694946ec64bbbda0b05ddba322581bdbccb9d3b
Isidro Merayo Castellano <imerayo@saludcastillayleon.es>
2012-08-09 12:48:15 +0200

    Dinamic fixtures and a bug no use max_jobs_on_homepage

--- src/Hcuv/JobeetBundle/Controller/JobController.php
+++ src/Hcuv/JobeetBundle/Controller/JobController.php
@@ -26,7 +26,9 @@ class JobController extends Controller
 
         foreach($categories as $category)
         {
-            $category->setActiveJobs($em->getRepository('HcuvJobeetBundle:Job')->getActiveJobs($category->getId()));
+            $category->setActiveJobs($em->getRepository('HcuvJobeetBundle:Job')
+                ->getActiveJobs($category->getId(),
+                    $this->container->getParameter('max_jobs_on_homepage')));
         }
 
         return $this->render('HcuvJobeetBundle:Job:index.html.twig', array(
--- src/Hcuv/JobeetBundle/DataFixtures/ORM/LoadJobData.php
+++ src/Hcuv/JobeetBundle/DataFixtures/ORM/LoadJobData.php
@@ -73,6 +73,24 @@ class LoadJobData extends AbstractFixture implements OrderedFixtureInterface
         $em->persist($job_extreme_sensio);
         $em->persist($job_expired);
 
+        for($i = 100; $i < 130; $i++)
+        {
+            $job = new Job();
+            $job->setCategory($em->merge($this->getReference('category-programming')));
+            $job->setType('full-time');
+            $job->setCompany('Company '.$i);
+            $job->setPosition('Web Developer');
+            $job->setLocation('Paris, France');
+            $job->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
+            $job->setHowToApply('Send your resume to lorem.ipsum [at] dolor.sit');
+            $job->setIsPublic(true);
+            $job->setIsActivated(true);
+            $job->setToken('job_'.$i);
+            $job->setEmail('job@example.com');
+
+            $em->persist($job);
+        }
+
         $em->flush();
     }
     /**
