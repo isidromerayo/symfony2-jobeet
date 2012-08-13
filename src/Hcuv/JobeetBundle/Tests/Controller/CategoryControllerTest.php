1e9199eeb71a80ab9d13fcd5bc7a15138c00cef0
Isidro Merayo Castellano <imerayo@saludcastillayleon.es>
2012-08-13 15:04:32 +0200

    functional test

--- src/Hcuv/JobeetBundle/Tests/Controller/CategoryControllerTest.php
+++ src/Hcuv/JobeetBundle/Tests/Controller/CategoryControllerTest.php
@@ -30,4 +30,27 @@ class CategoryControllerTest extends WebTestCase
         $this->assertEquals(1, $crawler->filter('html:contains("Programming")')->count());
         $this->assertEquals(1, $crawler->filter('div.pagination_desc:contains("page")')->count());
     }
+
+    public function testIndex()
+    {
+        $crawler = $this->client->request('GET', '/');
+        $this->assertEquals('Hcuv\JobeetBundle\Controller\JobController::indexAction',
+            $this->client->getRequest()->attributes->get('_controller'));
+        $this->assertTrue($crawler->filter('.jobs td.position:contains("Expired")')->count() == 0);
+    }
+
+    /**
+     * @test
+     */
+    public function onlyNJobsAreListedForACategory()
+    {
+        $crawler = $this->client->request('GET', '/');
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $max_jobs_on_homepage = $kernel->getContainer()->getParameter('max_jobs_on_homepage');
+        $this->assertTrue($crawler->filter('.category_programming tr')->count() == $max_jobs_on_homepage);
+        $this->assertTrue($crawler->filter('.category_design .more_jobs')->count() == 0);
+        $this->assertTrue($crawler->filter('.category_programming .more_jobs')->count() == 1);
+
+    }
 }
