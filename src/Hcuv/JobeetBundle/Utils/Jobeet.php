c2eb508290cac07b65f51af428253985debae0ce
Isidro Merayo Castellano <imerayo@saludcastillayleon.es>
2012-08-13 14:45:16 +0200

    UTF-8 encoding, as this is the default symfony encoding, and the one used by “iconv” to do the transliteration

--- src/Hcuv/JobeetBundle/Utils/Jobeet.php
+++ src/Hcuv/JobeetBundle/Utils/Jobeet.php
@@ -18,10 +18,18 @@ class Jobeet
      */
     static public function slugify($text)
     {
-        // replace all non letters or digits by -
-        $text = preg_replace('/\W+/', '-', $text);
-        // trim and lowercase
-        $text = strtolower(trim($text, '-'));
+        // replace non letter or digits by -
+        $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
+        // trim
+        $text = trim($text, '-');
+        // transliterate
+        if (function_exists('iconv')) {
+            $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        }
+        // lowercase
+        $text = strtolower($text);
+        // remove unwanted characters
+        $text = preg_replace('#[^-\w]+#', '', $text);
         if (empty($text)) {
             return 'n-a';
         }
