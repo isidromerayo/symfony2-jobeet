0b73505431072f2d0ca608a93806f195758e1e90
Isidro Merayo Castellano <imerayo@saludcastillayleon.es>
2012-08-13 17:14:55 +0200

    index functional

--- src/Hcuv/JobeetBundle/Tests/Controller/CategoryControllerTest.php
+++ src/Hcuv/JobeetBundle/Tests/Controller/CategoryControllerTest.php
@@ -33,11 +33,44 @@ class CategoryControllerTest extends WebTestCase
 
     public function testIndex()
     {
+        // get the custom parameters from app config.yml
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $max_jobs_on_homepage = $kernel->getContainer()->getParameter('max_jobs_on_homepage');
+        $max_jobs_on_category = $kernel->getContainer()->getParameter('max_jobs_on_category');
+
         $crawler = $this->client->request('GET', '/');
-        $this->assertEquals('Hcuv\JobeetBundle\Controller\JobController::indexAction',
-            $this->client->getRequest()->attributes->get('_controller'));
+        $this->assertEquals('Hcuv\JobeetBundle\Controller\JobController::indexAction', $this->client->getRequest()->attributes->get('_controller'));
+
+        // expired jobs are not listed
         $this->assertTrue($crawler->filter('.jobs td.position:contains("Expired")')->count() == 0);
-    }
+
+        // only $max_jobs_on_homepage jobs are listed for a category
+        $this->assertTrue($crawler->filter('.category_programming tr')->count() > 0);
+        $this->assertTrue($crawler->filter('.category_design .more_jobs')->count() == 0);
+        $this->assertTrue($crawler->filter('.category_programming .more_jobs')->count() == 1);
+
+        // jobs are sorted by date
+        $this->assertTrue($crawler->filter('.category_programming tr')->first()->filter(sprintf('a[href*="/%d/"]', $this->getMostRecentProgrammingJob()->getId()))->count() == 1);
+
+        // each job on the homepage is clickable and give detailed information
+        $job = $this->getMostRecentProgrammingJob();
+        $link = $crawler->selectLink('Web Developer')->first()->link();
+        $crawler = $this->client->click($link);
+        $this->assertEquals('Hcuv\JobeetBundle\Controller\JobController::showAction', $this->client->getRequest()->attributes->get('_controller'));
+        $this->assertEquals($job->getCompanySlug(), $this->client->getRequest()->attributes->get('company'));
+        $this->assertEquals($job->getLocationSlug(), $this->client->getRequest()->attributes->get('location'));
+        $this->assertEquals($job->getPositionSlug(), $this->client->getRequest()->attributes->get('position'));
+        $this->assertEquals($job->getId(), $this->client->getRequest()->attributes->get('id'));
+
+        // a non-existent job forwards the user to a 404
+        $crawler = $this->client->request('GET', '/job/foo-inc/milano-italy/0/painter');
+        $this->assertTrue(404 === $this->client->getResponse()->getStatusCode());
+
+        // an expired job page forwards the user to a 404
+        $crawler = $this->client->request('GET', sprintf('/job/sensio-labs/paris-france/%d/web-developer', $this->getExpiredJob()->getId()));
+        $this->assertTrue(404 === $this->client->getResponse()->getStatusCode());
+  }
 
     /**
      * @test
@@ -59,6 +92,30 @@ class CategoryControllerTest extends WebTestCase
     public function jobsAreSortedByDate()
     {
         $crawler = $this->client->request('GET', '/');
+        $this->assertTrue($crawler->filter('.category_programming tr')->first()->filter(sprintf('a[href*="/%d/"]', $this->getMostRecentProgrammingJob()->getId()))->count() == 1);
+    }
+    /**
+     * @test
+     */
+    public function eachJobOnTheHomePageIsClickable()
+    {
+        $crawler = $this->client->request('GET', '/');
+        $job = $this->getMostRecentProgrammingJob();
+        $link = $crawler->selectLink('Web Developer')->first()->link();
+        $crawler = $this->client->click($link);
+        $this->assertEquals('Hcuv\JobeetBundle\Controller\JobController::showAction',
+            $this->client->getRequest()->attributes->get('_controller'));
+        $this->assertEquals($job->getCompanySlug(),
+            $this->client->getRequest()->attributes->get('company'));
+        $this->assertEquals($job->getLocationSlug(),
+            $this->client->getRequest()->attributes->get('location'));
+        $this->assertEquals($job->getPositionSlug(),
+            $this->client->getRequest()->attributes->get('position'));
+        $this->assertEquals($job->getId(),
+            $this->client->getRequest()->attributes->get('id'));
+    }
+    public function getMostRecentProgrammingJob()
+    {
         $kernel = static::createKernel();
         $kernel->boot();
         $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
@@ -67,9 +124,16 @@ class CategoryControllerTest extends WebTestCase
         $query->setParameter('slug', 'programming');
         $query->setParameter('date', date('Y-m-d H:i:s', time()));
         $query->setMaxResults(1);
-        $job = $query->getSingleResult();
-
-        $this->assertTrue($crawler->filter('.category_programming tr')->first()->filter(sprintf('a[href*="/%d/"]', $job->getId()))->count() == 1);
+        return $query->getSingleResult();
     }
+    public function getExpiredJob()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
 
+        $query = $em->createQuery('SELECT j from HcuvJobeetBundle:Job j WHERE j.expires_at < :date');     $query->setParameter('date', date('Y-m-d H:i:s', time()));
+        $query->setMaxResults(1);
+        return $query->getSingleResult();
+    }
 }
