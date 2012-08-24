58dca7d3dd4c132e84379ffe283c39f1b287fb08
Isidro Merayo Castellano <imerayo@saludcastillayleon.es>
2012-08-24 12:58:22 +0200

    Configuration of Admin classes

--- src/Hcuv/JobeetBundle/Admin/CategoryAdmin.php
+++ src/Hcuv/JobeetBundle/Admin/CategoryAdmin.php
@@ -12,5 +12,23 @@ use Sonata\AdminBundle\Form\FormMapper;
  */
 class CategoryAdmin extends Admin
 {
+    protected $datagridValues = array(
+        '_sort_order' => 'ASC',
+        '_sort_by' => 'name'
+    );
 
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name')->add('slug');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name')->add('slug');
+    }
 }
--- src/Hcuv/JobeetBundle/Admin/JobAdmin.php
+++ src/Hcuv/JobeetBundle/Admin/JobAdmin.php
@@ -6,11 +6,86 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+use Hcuv\JobeetBundle\Entity\Job;
 /**
  *
  * User: imerayo
  */
 class JobAdmin  extends Admin
 {
+    // setup the defaut sort column and order
+    protected $datagridValues = array(
+        '_sort_order' => 'DESC',
+        '_sort_by' => 'created_at'
+    );
 
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('category')
+            ->add('type', 'choice', array('choices' => Job::getTypes(), 'expanded' => true))
+            ->add('company')
+            ->add('file', 'file', array('label' => 'Company logo', 'required' => false))
+            ->add('url')
+            ->add('position')
+            ->add('location')
+            ->add('description')
+            ->add('how_to_apply')
+            ->add('is_public')
+            ->add('email')
+            ->add('is_activated');
+    }
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('category')
+            ->add('company')
+            ->add('position')
+            ->add('description')
+            ->add('is_activated')
+            ->add('is_public')
+            ->add('email')
+            ->add('expires_at');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('company')
+            ->add('position')
+            ->add('location')
+            ->add('url')
+            ->add('is_activated')
+            ->add('email')
+            ->add('category')
+            ->add('expires_at')
+            ->add('_action', 'actions', array(
+            'actions' => array(
+                'view' => array(),
+                'edit' => array(),
+                'delete' => array(),
+            )
+        ));
+    }
+
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('category')
+            ->add('type')
+            ->add('company')
+            ->add('webPath', 'string', array('template' => 'HcuvJobeetBundle:JobAdmin:list_image.html.twig'))
+            ->add('url')
+            ->add('position')
+            ->add('location')
+            ->add('description')
+            ->add('how_to_apply')
+            ->add('is_public')
+            ->add('is_activated')
+            ->add('token')
+            ->add('email')
+            ->add('expires_at');
+    }
 }
