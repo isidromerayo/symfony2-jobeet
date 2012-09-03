c81188c97cb584cc49f3edbc66f83358251bd246
Isidro Merayo Castellano <imerayo@saludcastillayleon.es>
2012-09-03 11:27:10 +0200

    Securing the Application

---
+++ src/Hcuv/JobeetBundle/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+namespace Hcuv\JobeetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+/**
+ *
+ * User: imerayo
+ */
+class DefaultController extends Controller
+{
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('HcuvJobeetBundle:Default:login.html.twig', array(
+            // last username entered by the user
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        ));
+    }
+}
